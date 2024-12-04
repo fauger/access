596826006ccebfed18aea8a21d310c44480f6d52
fauger <fr.auger@gmail.com>
2024-12-04 13:43:52 +0100

    init

---
+++ assets/data.js
@@ -0,0 +1,65 @@
+const data = [{
+  "url": './assets/defrv788dgfg55.jpg',
+  "name": "TGV",
+  "details": "- 350 km/h<br> - 600 passagers<br> - 1988"
+}, 
+{
+  "url": './assets/def898dhh.jpg',
+  "name": "TER",
+  "details": "- 150 km/h<br> - 300 passagers<br> - 1967"
+},
+{
+  "url": './assets/dedfrfv7876frfrf.jpg',
+  "name": "Intercités",
+  "details": "- 200 km/h<br> - 800 passagers<br> - 1968"
+},
+{
+  "url": './assets/khoruvb8977.jpeg',
+  "name": "Transsibérien",
+  "details": "- 250 km/h<br> - 1000 passagers<br> - 1988"
+},
+{
+  "url": './assets/orient_express.jpg',
+  "name": "Orient Express",
+  "details": "- 350 km/h<br> - 600 passagers<br> - 1988"
+},
+{
+  "url": './assets/huukh7d8fr.png',
+  "name": "Ouigo",
+  "details": "- 350 km/h<br> - 600 passagers<br> - 2016"
+}, 
+{
+  "url": './assets/defr789hhfr.jpg',
+  "name": "Fret SNCF",
+  "details": "- 350 km/h<br> - 600 passagers<br> - 1988"
+}, 
+{
+  "url": './assets/d54Gd67hy.jpg',
+  "name": "Eurostar",
+  "details": "- 150 km/h<br> - 300 passagers<br> - 1967"
+},
+{
+  "url": './assets/d8jfjHGS78.jpg',
+  "name": "Poudlard Express",
+  "details": "- 200 km/h<br> - 800 passagers<br> - 1968"
+},
+{
+  "url": './assets/mpf89fd5s7.jpg',
+  "name": "Thalys",
+  "details": "- 250 km/h<br> - 1000 passagers<br> - 1988"
+},
+{
+  "url": './assets/d8DHS9shfHS.jpg',
+  "name": "RER A",
+  "details": "- 350 km/h<br> - 600 passagers<br> - 1988"
+},
+{
+  "url": './assets/bg8d0c.jpg',
+  "name": "Le train de Trotsky",
+  "details": "- 150 km/h<br> - 100 passagers<br> - 1917"
+},
+{
+  "url": './assets/snowpiercer.webp',
+  "name": "Le SnowPiercer",
+  "details": "- 1350 km/h<br> - 600 passagers<br> - 3016"
+}]
